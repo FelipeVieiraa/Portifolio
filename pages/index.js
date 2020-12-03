ad15d04edc48a5008742bd9c66793edf864861d3
felipevieiraa <felipezimba8228@gmail.com>
2020-12-02 22:36:00 -0300

    [Feature] getStaticProps test

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,35 @@
-function Home() {
-  return <div>Felipe Vieira</div>
+import Link from 'next/link';
+
+function Home(props) {
+  const dynamicDate = new Date();
+  const dynamicDateString = dynamicDate.toGMTString();
+
+  return (
+    <>
+    <div>Felipe Vieira</div>
+
+    <Link href="/sobre">
+      <a>Acessar pagina sobre.</a>
+    </Link>
+
+    <div>{dynamicDateString} (Dinâmico)</div>
+    <div>{props.StaticDateString} (Estático)</div>
+
+  </>
+  )
+
+
+}
+
+export function getStaticProps() {
+  const StaticDate = new Date();
+  const StaticDateString = StaticDate.toGMTString();
+
+  return{
+    props: {
+      StaticDateString
+    }
+  };
 }
 
 export default Home;
---
+++ pages/sobre.js
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+function Sobre() {
+  return (
+    <>
+    <div>Sobre</div>
+
+    <Link href="/">
+      <a>Acessar pagina Home.</a>
+    </Link>
+  </>
+  )
+
+
+}
+
+export default Sobre;
