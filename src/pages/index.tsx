638f5b3a5e62d5701daa85f2292e2044061ac0d3
felipevieiraa <felipezimba8228@gmail.com>
2020-12-03 17:08:16 -0300

    [Feature] add Contact

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Header from '../components/Header';
 import 'rbx/index.css';
 import { CSSProperties } from 'react';
-import { Tile, Section, Title, Image } from 'rbx';
+import { Tile, Section, Title, Image, Field, Label, Control, Input, Help, Select, Textarea, Checkbox, Radio, Button } from 'rbx';
 
 const IMAGE_CONTENT: CSSProperties = {
   display: "flex",
@@ -14,13 +14,13 @@ const MAIN: CSSProperties = {
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
-  flexDirection: "column",
-  gap: 50
+  flexDirection: "column"
 }
 
 const ABOUT_ME_CONTENT: CSSProperties = {
   width: "94vw",
-  background: "#010101"
+  background: "#010101",
+  paddingTop: 60
 }
 
 const TITLE: CSSProperties = {
@@ -48,14 +48,14 @@ const LINE_IN_SECTION: CSSProperties = {
   display: "flex",
   flexDirection: "column",
   justifyContent: "center",
-  paddingLeft: 5
+  paddingLeft: 10
 }
 
 const LINE_IN_SECTION_MARGIN_TOP: CSSProperties = {
   display: "flex",
   flexDirection: "column",
   justifyContent: "center",
-  paddingLeft: 5,
+  paddingLeft: 10,
   marginTop:50
 }
 
@@ -79,7 +79,16 @@ const WHAT_DO_I_DO_CONTENT: CSSProperties = {
   width: "94vw",
   background: "#fefefe",
   borderTopLeftRadius: 25,
-  borderTopRightRadius: 25
+  borderTopRightRadius: 25,
+  marginTop: 60
+}
+
+const CONTACT_CONTENT: CSSProperties = {
+  width: "94vw",
+  background: "#fefefe",
+  borderBottomLeftRadius: 25,
+  borderBottomRightRadius: 25,
+  paddingTop: 60
 }
 
 function Landing() {
@@ -110,7 +119,7 @@ function Landing() {
               </div>
 
               <div style={TITLE_2}>
-                <Title as="h2" subtitle style={{color: "#F9F9F9", textAlign: "center", paddingTop: 50, paddingBottom: 30}}>
+                <Title size={4} subtitle style={{color: "#F9F9F9", textAlign: "center", paddingTop: 50, paddingBottom: 30}}>
                   Minhas Skills
                 </Title>
 
@@ -159,11 +168,11 @@ function Landing() {
                 </Title> 
               </div>
 
-              <Tile kind="ancestor">
+              <Tile kind="ancestor" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
                   <Tile kind="parent" vertical>
 
                       <Tile kind="parent" horizontal>
-                        <Image src={"./app-image.png"} width="100"/>
+                        <Image src={"./app-image.png"} width="80"/>
                         <div style={LINE_IN_SECTION}>
                           <Title size={5}>Aplicativos</Title>
                           <Title subtitle>
@@ -173,7 +182,7 @@ function Landing() {
                       </Tile>
 
                       <Tile kind="parent" horizontal>
-                        <Image src={"./proto-image.png"} width="100"/>
+                        <Image src={"./proto-image.png"} width="80"/>
                         <div style={LINE_IN_SECTION}>
                           <Title size={5}>UX</Title>
                           <Title subtitle>
@@ -183,6 +192,79 @@ function Landing() {
                       </Tile>
                   </Tile>
                 </Tile>
+            </Section>
+          </div>
+
+          <div style={CONTACT_CONTENT}>
+            <div style={{ width: "98%", display: "flex", alignItems: "center", float: "right" }}>
+              <Image
+                src={'./logo.png'}
+                role="presentation"
+                width="115"
+              />
+              <div style={{ padding: 1, background: "#4a4a4a", flex: 1, marginTop: 3}}/>
+            </div>
+            
+            <Section>
+              <div style={{textAlign: "center", marginBottom: 50, marginTop: 50}}>
+                <Title size={3}>Fale Comigo</Title>
+                <Title size={6} subtitle>
+                  Resta alguma dúvida? Preencha os campos abaixo com os seguintes dados que em breve entrarei em contato.
+                </Title> 
+              </div>
+
+              <form style={{width: "90%", margin: "0 auto"}}>
+                <Field horizontal style={{gap: 20}}>
+                  <Field>
+                    <Label>Seu nome</Label>
+                    <Control>
+                      <Input type="text" placeholder="Digite seu nome..." />
+                    </Control>
+                  </Field>
+
+                  <Field>
+                    <Label>E-mail</Label>
+                    <Control iconLeft iconRight>
+                      <Input
+                        placeholder="email@exemplo.com"
+                        type="email"
+                      />
+                    </Control>
+                  </Field>
+                </Field>
+
+                <Field horizontal style={{gap: 20}}>
+                  <Field>
+                    <Label>Telefone</Label>
+                    <Control>
+                      <Input type="text" placeholder="(_ _)_ _ _ _ _ _ _ _" />
+                    </Control>
+                  </Field>
+
+                  <Field>
+                    <Label>Assunto</Label>
+                    <Control iconLeft iconRight>
+                      <Input
+                        placeholder="Digite aqui o assunto..."
+                        type="email"
+                      />
+                    </Control>
+                  </Field>
+                </Field>
+
+                <Field>
+                  <Label>Mensagem</Label>
+                  <Control>
+                    <Textarea placeholder="Digite sua mensagem..." />
+                  </Control>
+                </Field>
+
+                <Field style={{marginTop: 40}}>
+                  <Control style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "row" }}>
+                    <Button color="link">Enviar contato</Button>
+                  </Control>
+                </Field>
+              </form>
             </Section>
           </div>
         </div>
